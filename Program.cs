b241c07adf8668fdf1f26b1ad24baa437988dd14
Aidan Prehatny <aprehatny@yahoo.com>
2021-08-10 18:52:08 -0700

    Add some logic to signal for a windows toast notfication when audio playback is changed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿namespace switch_audio
+﻿using System;
+using System.Linq;
+
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace SwitchAudio
 {
-    using AudioSwitcher.AudioApi;
-    using AudioSwitcher.AudioApi.CoreAudio;
-    using System;
-    using System.Linq;
     class Program
     {
         private const string HEADSET = "Speakers (HyperX Cloud Flight S Game)";
@@ -14,35 +16,55 @@
             var ctlr = new CoreAudioController();
             var devices = ctlr.GetDevices(DeviceType.Playback, DeviceState.Active);
 
-            
+
             //  deviceType, AudioApi.Role role
             var currentDefault = ctlr.GetDefaultDevice(DeviceType.Playback, Role.Multimedia);
             var currentDefaultName = currentDefault.FullName;
             CoreAudioDevice headset = null;
             CoreAudioDevice speakers = null;
-            try {
-                 headset =
-                    (from d in devices
+            try
+            {
+                headset =
+                   (from d in devices
                     where d.FullName == HEADSET
                     select d).Single();
 
-                 speakers =
-                    (from d in devices
+                speakers =
+                   (from d in devices
                     where d.FullName == SPEAKERS
                     select d).Single();
-            } catch {
+            }
+            catch
+            {
                 Console.WriteLine("Single() call failed");
             }
 
-            if (currentDefaultName == HEADSET) {
+            CoreAudioDevice selectedDefault = null;
+
+            if (currentDefaultName == HEADSET)
+            {
                 Console.WriteLine($"Setting {HEADSET} as default device");
                 speakers.SetAsDefault();
                 speakers.SetAsDefaultCommunications();
-            } else if (currentDefaultName == SPEAKERS) {
+                selectedDefault = speakers;
+            }
+            else if (currentDefaultName == SPEAKERS)
+            {
                 Console.WriteLine($"Setting {SPEAKERS} as default device");
                 headset.SetAsDefault();
                 headset.SetAsDefaultCommunications();
+                selectedDefault = headset;
             }
+
+            ShortCutCreator.TryCreateShortcut("SwitchAudio.App", "SwitchAudio");
+
+            const string NOTIFICATION_IMG = @"C:\Users\apreh\Pictures\logic.png";
+
+            ToastActions.showImageToast(
+                "SwitchAudio.App",
+                $"Audio Switch occured - {DateTime.Now.ToLongTimeString()}",
+                $"Default Playback Device is now {selectedDefault.FullName}",
+                NOTIFICATION_IMG);
         }
     }
 }
